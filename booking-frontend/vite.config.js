d897e1023c7ec6032a3db5bc2dd2a400565cb613
matitas2004 <matiriveraam84@gmail.com>
2026-04-28 13:22:23 -0500

    Subiendo configuracion de microservicios

--- booking-frontend/vite.config.js
+++ booking-frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5028',
+        target: 'http://127.0.0.1:5028',
         changeOrigin: true,
         secure: false,
       }
